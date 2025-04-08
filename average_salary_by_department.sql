eaa838248f9b942d29e645f665563fd40ab1c8e1
VijayMore <vijaymore300793@gmail.com>
2025-04-08 19:48:09 +0530

    Delete average_salary_by_department.sql

--- average_salary_by_department.sql
+++
@@ -1,5 +0,0 @@
-SELECT Department, AVG(`Annual Salary`) AS avg_annual_salary
-FROM `salaries_data.chicago_salaries`
-WHERE `Annual Salary` IS NOT NULL
-GROUP BY Department
-ORDER BY avg_annual_salary DESC;
