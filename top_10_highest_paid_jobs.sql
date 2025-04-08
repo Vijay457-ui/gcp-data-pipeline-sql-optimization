c4874e271f27b0db7adb386d2e9b2987bc2fee66
VijayMore <vijaymore300793@gmail.com>
2025-04-08 19:49:28 +0530

    Delete top_10_highest_paid_jobs.sql

--- top_10_highest_paid_jobs.sql
+++
@@ -1,6 +0,0 @@
-SELECT `Job Titles`, AVG(`Annual Salary`) AS avg_salary
-FROM `salaries_data.chicago_salaries`
-WHERE `Annual Salary` IS NOT NULL
-GROUP BY `Job Titles`
-ORDER BY avg_salary DESC
-LIMIT 10;
