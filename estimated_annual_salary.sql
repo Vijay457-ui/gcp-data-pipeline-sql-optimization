e08ff6bffa36a445398ace4690a529e6881297cd
VijayMore <vijaymore300793@gmail.com>
2025-04-08 19:48:24 +0530

    Delete estimated_annual_salary.sql

--- estimated_annual_salary.sql
+++
@@ -1,4 +0,0 @@
-SELECT Name, `Hourly Rate`, `Typical Hours`,
-       ROUND(`Hourly Rate` * `Typical Hours` * 52, 2) AS estimated_annual_salary
-FROM `salaries_data.chicago_salaries`
-WHERE `Hourly Rate` IS NOT NULL AND `Typical Hours` IS NOT NULL;
